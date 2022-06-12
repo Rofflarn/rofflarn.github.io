1107bfa0ac627be1159e5c55fa08bfe48b5ee399
Robert Linder Blomberg <rblomberg@sandvine.com>
2022-06-12 20:46:12 +0200

    Update seats in parlament and remove Fi

--- leafletembed.js
+++ leafletembed.js
@@ -17,7 +17,6 @@ function initmap() {
 	map.addLayer(osm);
 
 	var partyC = loadMarkersForC();
-	var partyFI = loadMarkersForFI();
 	var partyKd = loadMarkersForKd();
 	var partyL = loadMarkersForL();
 	var partyMp = loadMarkersForMp();
@@ -27,7 +26,6 @@ function initmap() {
 	var partyV = loadMarkersForV();
 
 	map.addLayer(partyC);
-	map.addLayer(partyFI);
 	map.addLayer(partyKd);
 	map.addLayer(partyL);
 	map.addLayer(partyMp);
@@ -37,14 +35,13 @@ function initmap() {
 	map.addLayer(partyV);
 
 	var parties = {
-	    "<img width=8% src=\"gfx/logo_c.png\"> Centerpartiet, 4": partyC,
-            "<img width=8% src=\"gfx/logo_fi.png\"> Feministiskt iniativ, 0": partyFI,
-	    "<img width=8% src=\"gfx/logo_kd.jpg\"> Kristdemokraterna, 2": partyKd,
+	    "<img width=8% src=\"gfx/logo_c.png\"> Centerpartiet, 5": partyC,
+	    "<img width=8% src=\"gfx/logo_kd.jpg\"> Kristdemokraterna, 3": partyKd,
             "<img width=8% src=\"gfx/logo_l.png\"> Liberalerna, 5": partyL,
-	    "<img width=8% src=\"gfx/logo_mp.png\"> Miljöpartiet, 6": partyMp,
-	    "<img width=8% src=\"gfx/logo_m.png\"> Moderaterna, 13": partyM,
-	    "<img width=8% src=\"gfx/logo_s.png\"> Socialdemokraterna, 13": partyS,
-	    "<img width=8% src=\"gfx/logo_sd.png\"> Sverigedemokraterna, 6": partySD,
+	    "<img width=8% src=\"gfx/logo_mp.png\"> Miljöpartiet, 3": partyMp,
+	    "<img width=8% src=\"gfx/logo_m.png\"> Moderaterna, 14": partyM,
+	    "<img width=8% src=\"gfx/logo_s.png\"> Socialdemokraterna, 11": partyS,
+	    "<img width=8% src=\"gfx/logo_sd.png\"> Sverigedemokraterna, 7": partySD,
             "<img width=8% src=\"gfx/logo_v.png\"> Vänsterpartiet, 3": partyV,
 	};
 
